3ac8cc8549b42081f49846e278eaa6c7931b457e
mschodin <michael-schodin@uiowa.edu>
2020-03-03 17:02:05 -0600

    Messing with login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './App';
+import NavBar from './NavBar';
+
+ReactDOM.render(
+  //<h1>Hello, world! THe number you are looking for is: </h1>,
+  // <App />,
+  <NavBar />,
+  document.getElementById('root')
+);
 
 ReactDOM.render(
   //<h1>Hello, world! THe number you are looking for is: </h1>,
   <App />,
+  // <NavBar />,
   document.getElementById('root')
 );
