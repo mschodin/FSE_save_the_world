39bc5c5be9c889795121fb83e32f6e5fa3646c09
mschodin <michael-schodin@uiowa.edu>
2020-03-13 12:43:51 -0500

    Working on web tokens, just installed jwt

--- backend/routes/submitLogin.js
+++ backend/routes/submitLogin.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var router = express.Router();
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 router.get('/', function(req,res) {
     res.send("Welcome Tester!");
 });
 
+router.post('/exp', function(req, res) {
+    const { email, password } = req.body;
+})
 
 module.exports = router;
