140b8dafcdf6571283856986b3743f02e0496328
Nicholas Reid <nicreid1999@gmail.com>
2020-04-19 19:21:14 +0000

    Update itemsDonate.js

--- Database/itemsDonate.js
+++ Database/itemsDonate.js
@@ -18,7 +18,7 @@ con.query(sql, (error, results, fields) => {
     }
 })
 
-function viewRequests(itemName='*', location='*', amount='*', email='*'){
+function viewDonations(itemName='*', location='*', amount='*', email='*'){
     let sql = 'SELECT * FROM itemdonations WHERE itemName =' + mysql.escape(itemName) + "AND location =" + mysql.escape(location) +
                 "AND amount = " + mysql.escape(amount) + "AND email = " + mysql.escape(email);
 
@@ -29,7 +29,7 @@ function viewRequests(itemName='*', location='*', amount='*', email='*'){
     })
 }
 
-function addRequest(itemName, location, amount, email){
+function addDonations(itemName, location, amount, email){
     let sql ='INSERT INTO savetheworld.itemdonations(itemName,location,amount,email) VALUES('+
             mysql.escape(itemName)+','+mysql.escape(location)+','+mysql.escape(amount)+','+mysql.escape(email)+')';
 
@@ -41,12 +41,22 @@ function addRequest(itemName, location, amount, email){
     console.log("Donation Registered");
 }
 
-function removeRequest(iditemRequest){
-    let sql = 'DELETE FROM savetheworld.itemdonations WHERE iditemDonate =' + mysql.escape(iditemRequest);
+function removeDonations(iditemDonate){
+    let sql = 'DELETE FROM savetheworld.itemdonations WHERE iditemDonate =' + mysql.escape(iditemDonate);
     con.query(sql, (error, results, fields) => {
         if (error) {
             return console.error(error.message);
         }
     })
     console.log("Donation Removed");
+}
+
+function subtractDonations(iditemDonate, subtract){
+    let sql = 'UPDATE savetheworld.itemdonations SET amount = amount - ' + mysql.escape(subtract) + 'WHERE iditemDonate = ' + mysql.escape(iditemDonate);
+    con.query(sql, (error, results, fields) => {
+        if (error) {
+            return console.error(error.message);
+        }
+    })
+    console.log("Donation subtracted");
 }
